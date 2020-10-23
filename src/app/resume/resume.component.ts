ff6ffca56aa7d42bbdb99e734a3bf3e265df693e
Kamran Sadiq <kamransadiq111@hotmail.com>
2020-10-24 04:21:48 +0500

    latet build

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-resume',
@@ -12,4 +14,18 @@ export class ResumeComponent implements OnInit {
   ngOnInit() {
   }
 
+  generatePDF(){
+    var data = document.getElementById('kamran-sadiq');
+    html2canvas(data,{
+      scale: 2
+    }).then(canvas => {
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jsPDF('p', 'mm', 'a4');
+      var width = pdf.internal.pageSize.getWidth();
+      var height = pdf.internal.pageSize.getHeight();
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, width, height)
+      pdf.save('kamran-sadiq resume');
+    });
+  }
+  
 }
